7f9be4cd628b6aab6a1ca37288e44ad3fe8ca159
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-08-06 23:21:26 -0700

    [added] challenge 5

--- Challenges/challenge_5.py
+++ Challenges/challenge_5.py
@@ -1,1 +1,4 @@
-# Traveling Salesman Problem
+# Using your Graph ADT code, or in a completely separate program
+# (without a graph class). Determine if a given undirected graph 
+# is Eulerian (has an Eulerian Cycle). Your code should be well 
+# documented, tested and organized
